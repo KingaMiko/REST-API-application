9a48833e0c0eacda2c08b77c83ae4e87f0685ed6
Kinga Mikołajczyk <kinga.mikola@gmail.com>
2023-10-27 13:24:28 +0200

    Connect to MongoDB using Mongoose

--- app.js
+++ app.js
@@ -6,19 +6,21 @@ import contactsRouter from "./routes/api/contacts.js";
 const app = express();
 
 const formatsLogger = app.get("env") === "development" ? "dev" : "short";
-
-app.use(logger(formatsLogger));
-app.use(cors());
 app.use(express.json());
-
+app.use(cors());
+app.use(logger(formatsLogger));
 app.use("/api/contacts", contactsRouter);
 
 app.use((req, res) => {
-  res.status(404).json({ message: "Not found" });
+  res.status(404).json({ message: `whooops - ${req.path}` });
 });
 
 app.use((err, req, res) => {
-  res.status(500).json({ message: err.message });
+  if (err.name === "ValidationError") {
+    res.status(400).json({ message: err.message });
+  } else {
+    res.status(500).json({ message: err.message });
+  }
 });
 
 export default app;
--- server.js
+++ server.js
@@ -1,5 +1,23 @@
 import app from "./app.js";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000");
-});
+const { DB_HOST: uriDb } = process.env;
+const connection = mongoose.connect(uriDb);
+
+async function startServer() {
+  try {
+    await connection;
+    console.log("Database connection successful");
+    app.listen(3000, function () {
+      console.log("Server running. Use our API on port: 3000");
+    });
+  } catch (err) {
+    console.log("Database connection failed, shutting down");
+    console.error(err);
+    process.exit(1);
+  }
+}
+
+startServer();
