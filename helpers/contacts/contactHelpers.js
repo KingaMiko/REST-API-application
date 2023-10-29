6921295f70b76026e4de35350193550891867734
Kinga Mikołajczyk <kinga.mikola@gmail.com>
2023-10-29 11:58:24 +0100

    Refactor CRUD operations to use Mongoose/deleteContacts.js

--- helpers/contacts/contactHelpers.js
+++ helpers/contacts/contactHelpers.js
@@ -6,3 +6,6 @@ export const fetchContact = (contactId) => Contact.findById(contactId);
 
 export const addContact = ({ name, email, phone }) =>
   Contact.create({ name, email, phone });
+
+export const removeContact = (contactId) =>
+  Contact.deleteOne({ _id: contactId });
