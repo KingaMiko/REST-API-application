8d0af8ac8751bacf7c7ce5bc9ae882d44f8aba4e
Kinga Mikołajczyk <kinga.mikola@gmail.com>
2023-11-20 12:10:42 +0100

    Added email registration verification template

--- email/emailSender.js
+++ email/emailSender.js
@@ -1,19 +1,33 @@
 import sgMail from "@sendgrid/mail";
 import dotenv from "dotenv";
-dotenv.config();
+import handlebars from "handlebars";
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
 
+dotenv.config();
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const loadTemplate = (templateName, data) => {
+  const filePath = path.join(__dirname, "templates", `${templateName}.hbs`);
+  const source = fs.readFileSync(filePath, "utf-8");
+  const template = handlebars.compile(source);
+  return template(data);
+};
 
 export async function sendVerificationEmail(userEmail, verificationToken) {
-  const baseURL = process.env.BASE_URL;
+  const baseURL = process.env.BASE_URL || "http://localhost:3000";
   const verificationUrl = `${baseURL}/api/users/verify/${verificationToken}`;
+
+  const emailContent = loadTemplate("verificationEmail", {
+    verificationUrl,
+  });
 
   const message = {
     to: userEmail,
     from: "my.voice.evaporates@gmail.com",
     subject: "Email Verification for Your Account",
-    text: `Please verify your email by clicking on this link: ${verificationUrl}`,
-    html: `<strong>Please verify your email by clicking on this link:</strong> <a href="${verificationUrl}">${verificationUrl}</a>`,
+    html: emailContent,
   };
 
   await sgMail.send(message);
